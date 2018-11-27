bfbf3b6eb296920d56858a0b8673fa76f9fdf5a3
juliano1903 <llara.juliano@gmail.com>
2018-11-27 20:38:52 -0200

    Componente com arrow function

---
+++ exercicios_react/ex.1/componente.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+    <h1>Primeira Função</h1>
+)
---
+++ exercicios_react/ex.1/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Componente from './componente.jsx'
+
+ReactDOM.render(<Componente/>, document.getElementById('app'))
